7bdfb12fcfd6078124bdcb32b63dabac501aea4d
Mihyun <douob0@naver.com>
2020-08-12 09:54:04 +0900

    practice

---
+++ map/Ex10_11.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class Ex10_11 {
+	public static void main(String[] args) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", "홍길동");
+		map.put("age", "20");
+		map.put("adress", "서울");
+		map.put("email", "null");
+		
+		System.out.println("이름은 : "+map.get("name"));
+		System.out.println("나이는 : "+map.get("age"));
+		System.out.println("주소는 : "+map.get("name"));
+		System.out.println("email은 : "+map.get("email"));
+		
+		System.out.println("데이터의 크기(길이)는? : "+map.size());
+		System.out.println("이름은 : "+map.isEmpty());
+		System.out.println("age 키가 있나? : "+map.containsKey("age"));
+		System.out.println("홍길동 값이 있나? : "+map.containsValue("홍길동"));
+		System.out.println("age 키에 해당하는 데이터 삭제");
+		map.remove("age");
+		
+		System.out.println("전체 데이터 출력: "+map);
+	}
+}
