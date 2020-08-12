6bd6bed895559a6b329887f6aa628eaacc789d8c
Mihyun <douob0@naver.com>
2020-08-12 10:22:46 +0900

    Create MapTest2

---
+++ map/PersonMapTest2.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class PersonMapTest2 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Person> list1 = new ArrayList<Person>();
+		list1.add(new Person("홍길동", 20, "서울"));
+		list1.add(new Person("홍길동2", 30, "서울2"));
+		list1.add(new Person("홍길동3", 40, "서울3"));
+		
+		ArrayList<Person> list2 = new ArrayList<Person>();
+		list1.add(new Person("이순신", 20, "경기"));
+		list1.add(new Person("이순신2", 30, "경기2"));
+		list1.add(new Person("이순신3", 40, "경기3"));
+		
+		
+		
+		
+	}
+
+}
