2845250de17b424a153356f7b16e89b561d93eb3
xianeml <xianemml@gmail.com>
2020-12-13 14:45:14 +0900

    Spring MVC project - @Controller & Junit Tests

---
+++ Spring/zerock-springMVC-part2/ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,63 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor // DI: 생성자 자동주입 또는 @Setter 사용
+public class BoardController {
+
+	private BoardService service;
+
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}
+
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register: " + board);
+		service.register(board);
+		// list에 새로 등록된 게시물번호도 같이 들고가기
+		rttr.addFlashAttribute("result", board.getBno());
+		// response.sendRedirect() 처리
+		return "redirect:/board/list";
+	}
+
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+	}
+
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify: " + board);
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove........" + bno);
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+}
---
+++ Spring/zerock-springMVC-part2/ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -0,0 +1,89 @@
+package org.zerock.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration // Test for Controller
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@Log4j
+public class BoardControllerTests {
+
+	@Setter(onMethod_ = {@Autowired})
+	private WebApplicationContext ctx;
+	
+	// 가짜 mvc. URL과 파라미터 등을 브라우저에서 사용하는 것처럼 만들어 Controller 테스트
+	private MockMvc mockMvc;
+	
+	// 모든 테스트 전에 매번 실행됨
+	@org.junit.Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception{
+		log.info(
+				mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());  // 모델에 담겨있는 데이터 확인
+	}
+	
+	@Test
+	public void testRegister() throws Exception{
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+				// 전송할 파라미터 지정 가능
+				.param("title", "테스트 새 글제목")
+				.param("content", "테스트 새 글내용")
+				.param("writer", "user00"))
+				.andReturn()
+				.getModelAndView()
+				.getViewName();
+		log.info(resultPage);
+	}
+
+	@Test
+	public void testGet() throws Exception{
+		log.info(
+				mockMvc.perform(MockMvcRequestBuilders.get("/board/get")
+				.param("bno", "2"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
+	
+	@Test
+	public void testModify() throws Exception{
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify")
+				.param("bno", "10")
+				.param("title", "수정된 테스트 새 글제목")
+				.param("content", "수정된 테스트 새 글내용")
+				.param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+
+	@Test
+	public void testRemove() throws Exception{
+		// 삭제전 db bno 확인
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+				.param("bno", "25")
+				).andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+}
